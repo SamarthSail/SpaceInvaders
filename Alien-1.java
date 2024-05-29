a8973e08729af66c5b584db524108896aad2f0de
SamarthSail <149702121+SamarthSail@users.noreply.github.com>
2024-05-29 16:53:17 +0000

    Add files via upload

---
+++ Alien-1.java
@@ -0,0 +1,43 @@
+import java.awt.Image;
+
+public class Alien extends Sprite2D {
+    private static double xSpeed = 0;
+    private boolean isAlive = true; // Flag to determine if alien is alive
+
+    public Alien(Image i1, Image i2) {
+        super(i1, i2); // invoke constructor on superclass Sprite2D
+    }
+
+    // public interface
+    public boolean move() {
+        x += xSpeed;
+
+        // direction reversal needed?
+        
+        if (x <= 0 || x >= winWidth - myImage1.getWidth(null)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void setFleetXSpeed(double dx) {
+        xSpeed = dx;
+    }
+
+    public static void reverseDirection() {
+        xSpeed = -xSpeed;
+    }
+
+    public void jumpDownwards() {
+        y += 20;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+}
